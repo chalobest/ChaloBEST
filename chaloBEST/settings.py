a0e6c6a9f812ebe7ba230624c9369edb407f817f
Sanj <b@pad.ma>
2011-12-15 18:29:44 +0530

    path to sqlite db shud be absolute

--- chaloBEST/settings.py
+++ chaloBEST/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'chalobest.db',                      # Or path to database file if using sqlite3.
+        'NAME': join(PROJECT_ROOT, 'chalobest.db'),                      # Or path to database file if using sqlite3.
         'USER': 'sanj',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.

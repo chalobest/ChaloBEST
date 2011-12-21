033486ed884ea933fcefb357d6e02830022c38f3
Sanj <b@pad.ma>
2011-12-21 17:22:26 +0530

    index page cruft

--- chaloBEST/urls.py
+++ chaloBEST/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^chaloBEST/', include('chaloBEST.foo.urls')),
-
+    (r'^$', 'mumbai.views.index'),
     # Uncomment the admin/doc line below to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 

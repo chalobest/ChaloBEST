ea98294afd13751e144e16bb25dc68ff16ea477c
Sanj <b@pad.ma>
2011-12-29 19:14:29 +0530

    merged, fixed image in template

--- chaloBEST/urls.py
+++ chaloBEST/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
-
+import settings
+from os.path import join
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -15,3 +16,12 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.LOCAL_DEVELOPMENT:
+#
+  urlpatterns += patterns('',
+#
+  (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': join(settings.PROJECT_ROOT, "static")}),
+#
+)
+

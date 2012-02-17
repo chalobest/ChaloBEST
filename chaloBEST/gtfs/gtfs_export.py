c56252a85b2a88c4e1ed1c367e84b8fc928381a8
Johnson Chetty <johnsonchetty@gmail.com>
2012-02-17 21:38:10 +0100

    gtfs v5 in progress trips and stop_times..

--- chaloBEST/gtfs/gtfs_export.py
+++ chaloBEST/gtfs/gtfs_export.py
@@ -32,7 +32,7 @@ def export_routes(routebeer):
     for route in routebeer:
         try:
             # data checks here
-            filedude.writerow([route.id,route.alias,route.from_stop_txt + " - " + route.to_stop_txt,3])
+            filedude.writerow([route.code,route.alias[0:3],route.from_stop_txt + " - " + route.to_stop_txt,3])
         except:
             pass
 
@@ -44,7 +44,7 @@ def export_stops(olist):
             # data checks here 
             # stop_code is used for stop_id as its BEST specfic..
             # 
-            filedude.writerow([stop.code,stop.name,stop.point.coords[1],stop.point.coords[0]])
+            filedude.writerow([stop.id,stop.name,stop.point.coords[1],stop.point.coords[0]])
         except:
             pass
 
@@ -52,18 +52,18 @@ def export_agency():
     filedude = csv.writer(open(join(PROJECT_ROOT, "gtfs/agency.txt"), "w"), delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     # also
-    #filedude.writerow(["agency_id", "agency_name","agency_url","agency_timezone","agency_lang"])
-    #filedude.writerow(["BEST","BrihanMumbai Electric Supply & Transport","http://www.bestundertaking.com/","Asia/Kolkata","en"])
+    filedude.writerow(["agency_id", "agency_name","agency_url","agency_timezone","agency_lang"])
+    filedude.writerow(["BEST","BrihanMumbai Electric Supply & Transport","http://www.bestundertaking.com/","Asia/Kolkata","en"])
 
-    filedude.writerow(["agency_id" ,"agency_name","agency_url","agency_timezone"])
-    filedude.writerow([1 ,"BEST","www.chalobest.in","Asia/Kolkata"])
+    #filedude.writerow(["agency_id" ,"agency_name","agency_url","agency_timezone"])
+    #filedude.writerow([1 ,"BEST","www.chalobest.in","Asia/Kolkata"])
 
 
             # stop_code is used for stop_id as its BEST specfic..
 
 SERVICE_SCHEDULE = [
     {'id':0,'code':'MS','days':[1,2,3,4,5,6]},
-    {'id':1,'code':'HOL','days':[8]},
+    {'id':1,'code':'HOL','days':[7,8]}, # should be only 8
     {'id':2,'code':'SUN','days':[7]},
     {'id':3,'code':'MF&HOL','days':[1,2,3,4,5,8]},
     {'id':4,'code':'SAT','days':[6]},
@@ -115,18 +115,18 @@ def export_calendar():
 def export_trips():
     filedude = csv.writer(open(join(PROJECT_ROOT, "gtfs/trips.txt"), "w"), delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
     filedude.writerow(["route_id","service_id","trip_id"])
-    filedude.writerow(["246","MS","282_1"])
+    filedude.writerow(["2820","MS","2820_1"])
     #filedude.writerow(["246","HOL","282_2"])
     #filedude.writerow(["246","SUN","282_3)"])
 
-    filedude.writerow(["253","MS","289_1"])
+    filedude.writerow(["2894","MS","2894_1"])
     #filedude.writerow(["253","HOL","289_2"])
     #filedude.writerow(["253","SUN","289_3"])
 
     # we need to get UniqueRoutes for each route, that is one trip, since it is based on service_id which shows days_of_run.
     # we need to be careful here because a filter queryset for UniqueRoutes can differ in order and a naming based on this order 
     # will not be consistent. Its good to use a uniqueroute-serial number.
-
+   for r in routelist: 
     """
         try:
             # data checks here 
@@ -149,17 +149,67 @@ def export_trips():
 
                
         
-def export_stop_times():
+def export_stop_times(routelist):
     filedude = csv.writer(open(join(PROJECT_ROOT, "gtfs/stop_times.txt"), "w"), delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    filedude.writerow(["trip_id","arrival_time","departure_time","stop_id","stop_sequence"])    
-    #routelist = getRoutesHavingAllLocs()
-    routelist = RouteDetails.objects.filter(route_code='2820')
-    routelist.append(RouteDetails.objects.get(route_code='2894'))
+    filedude.writerow(["trip_id","arrival_time","departure_time","stop_id","stop_sequence"])
+    #routelist = getRoutesHavingAllLocs()    
 
     #1. get routeDetails
     #2. 
     for r in routelist:
-        rds = RouteDetail.objects.filter(route=r).order_by('serial')
-        
+        rds = RouteDetail.objects.filter(route=r).order_by('serial')        
         for rd in rds:            
-            filedude.writerow()
+            filedude.writerow([r.code+"_1","","",rd.stop.id,rd.serial])
+
+
+
+"""
+stop_times.txt
+================================================================================================================================================
+1. For each route.
+2. Get rdlist = routedetails for that route.order_by('serial'). Get UniqueRoutes for the route.
+3. --- scenario -- Not considering uniqueroutes--
+
+3.1 For rd in rdlist
+3.1.1 filewrite (trip_id,,,stopid,stop.serial)
+
+----alternate scenario
+3. For each UniqueRoute, get from_to stops list (rdsubset) from RouteDetail list
+for unr in unrs:
+from_stop, to_stop
+rd_subset  = rdlist[getserial(rdlist,from_stop):getserial(rdlist,to_stop)]
+"""
+
+
+def export_frequencies():
+    filedude = csv.writer(open(join(PROJECT_ROOT, "gtfs/frequencies.txt"), "w"), delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    """
+    EACH ROW IN FREQUENCIES
+    - For an entry in atlas, [ i.e. a given trip+service_id [subset + schedule days] ] 
+    If there are headway timings for diff time slots, eg. 
+    h7-11, h11-16, h16-22,h22-25
+    
+    then
+    """
+    
+    filedude.writerow(["trip_id", "start_time","end_time","headway_secs"])
+    filedude.writerow(["282_1","07:00:00","11:00:00",360])
+    filedude.writerow(["282_1","11:00:00","16:00:00",420])
+    filedude.writerow(["282_1","16:00:00","22:00:00",480])
+    filedude.writerow(["282_1","07:00:00","11:00:00",360])
+    filedude.writerow(["289_1","07:00:00","11:00:00",600])
+    filedude.writerow(["289_1","11:00:00","16:00:00",420])
+    filedude.writerow(["289_1","16:00:00","22:00:00",420])
+    filedude.writerow(["289_1","07:00:00","11:00:00",540])
+    
+
+def getserial(rdlist,stop):
+    #check if rdlist is of a ring route..
+    if rdlist[0].route.code[3]== 'R' or '4' :
+        # write ring specific code here. rings have multiple occuring stops, which one to choose??
+        pass
+
+    for rd in rdlist:
+        if(rd.stop==stop):
+            return rdlist.index(rd)
+    

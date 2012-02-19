723e18910c00d858923807e24ecca68b3d870202
Sanj <b@pad.ma>
2012-02-19 18:41:19 +0530

    change stop requests to slugs instead of ids

--- chaloBEST/mumbai/apiurls.py
+++ chaloBEST/mumbai/apiurls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 import apiviews
 
 urlpatterns = patterns('',
-    (r'^route/(?P<alias>.*)$', apiviews.route), #FIXME: better regexp for route alias?
-    (r'^stop/(?P<id>[0-9]*)$', apiviews.stop),
+    (r'^route/(?P<slug>.*)$', apiviews.route), #FIXME: better regexp for route alias?
+    (r'^stop/(?P<slug>.*)$', apiviews.stop),
 )
--- chaloBEST/mumbai/apiviews.py
+++ chaloBEST/mumbai/apiviews.py
@@ -3,8 +3,8 @@ from ox.django.shortcuts import get_object_or_404_json, render_to_json_response
 from django.contrib.auth.decorators import login_required
 
 
-def route(request, alias):
-    route = get_object_or_404_json(Route, alias=alias)
+def route(request, slug):
+    route = get_object_or_404_json(Route, slug=slug)
     stops = [r.stop.get_geojson() for r in RouteDetail.objects.filter(route=route)]
     return render_to_json_response({
         'route': route.get_dict(),
@@ -15,13 +15,13 @@ def route(request, alias):
     })
 
 
-def stop(request, id):
+def stop(request, slug):
     if request.POST:
-        if not id:
+        if not slug:
             stop = Stop() #FIXME: should this return an error instead?
         else:
-            stop = get_object_or_404_json(Stop, id=id)
+            stop = get_object_or_404_json(Stop, slug=slug)
         return render_to_json_response(stop.from_geojson(request.POST))
     else:
-        stop = get_object_or_404_json(Stop, id=id)
+        stop = get_object_or_404_json(Stop, slug=slug)
         return render_to_json_response(stop.get_geojson()) #FIXME: please don't repeat this code, its retarded.

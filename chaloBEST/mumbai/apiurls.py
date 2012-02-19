d5f9d63ae9afbdfac0ab28b1f39aab03a4190f6d
Sanj <b@pad.ma>
2012-02-19 20:01:00 +0530

    add call for area

--- chaloBEST/mumbai/apiurls.py
+++ chaloBEST/mumbai/apiurls.py
@@ -3,6 +3,7 @@ import apiviews
 
 urlpatterns = patterns('',
     (r'^route/(?P<slug>.*)$', apiviews.route), #FIXME: better regexp for route alias?
+    (r'^area/(?P<slug>.*)$', apiviews.area),
     (r'^stop/(?P<slug>.*)$', apiviews.stop),
     (r'^routes/$', apiviews.routes),
     (r'^areas/$', apiviews.areas),

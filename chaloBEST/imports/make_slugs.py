d5fbfc4edd85ffc699617c84cb695543dbcb4fa6
Sanj <b@pad.ma>
2012-02-20 19:40:22 +0530

    update slug script to ensure unique / ugly hack

--- chaloBEST/imports/make_slugs.py
+++ chaloBEST/imports/make_slugs.py
@@ -4,7 +4,10 @@ from mumbai.models import *
 def do():
     for cls in [Road, Area, Stop]:
         for obj in cls.objects.all():
-            obj.slug = slugify(obj.display_name)
+            slug = slugify(obj.display_name)
+            if cls.objects.filter(slug=slug).count() > 1:
+                slug += "2"
+            obj.slug = slug
             obj.save()
     for r in Route.objects.all():
         r.slug = r.alias

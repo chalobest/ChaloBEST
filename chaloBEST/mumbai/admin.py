89fc6d28a51b2c5b04b5e7594c496d468a5e6daa
Johnson Chetty <johnsonchetty@gmail.com>
2012-10-05 12:45:42 +0200

    holiday tweaks 3

--- chaloBEST/mumbai/admin.py
+++ chaloBEST/mumbai/admin.py
@@ -195,7 +195,7 @@ class DepotAdmin(admin.ModelAdmin):
 
 class HolidayAdmin(admin.ModelAdmin):
     list_display = ("date", "name","operating_schedule") 
-    list_editable = ("operating_schedule") 
+    list_editable = ("operating_schedule",) 
     readonly_fields =  ("date", "name")    
     search_fields =  ("name", "date")
     formfield_overrides = {

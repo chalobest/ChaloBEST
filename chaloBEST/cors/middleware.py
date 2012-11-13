c9720421949bd2c50fcb54c4af829672e80908af
Sanj <b@pad.ma>
2012-11-13 16:42:12 +0530

    add cors to main dir, edit headers for cross origin

---
+++ chaloBEST/cors/middleware.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+
+class AllowOriginMiddleware(object):
+    def process_request(self, request):
+        if request.method == 'OPTIONS':
+            return HttpResponse()
+
+    def process_response(self, request, response):
+        origin = request.META.get('HTTP_ORIGIN')
+        if origin:
+            response['Access-Control-Allow-Origin'] = origin
+            response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, DELETE, PUT'
+            response['Access-Control-Allow-Headers'] = 'Content-Type, Accept'
+        return response

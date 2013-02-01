15f2fa90d6b71638d4f4948a83f43f905975c2df
Sanjay B <b@pad.ma>
2013-02-01 20:18:34 +0530

    add edit in OSM plugin, fix minimap bug

--- chaloBEST/mumbai/static/js/index.js
+++ chaloBEST/mumbai/static/js/index.js
@@ -4,9 +4,20 @@ $(function() {
     var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     var osmAttrib = 'Map data © openstreetmap contributors'
     var osm = new L.TileLayer(osmUrl, {minZoom:1,maxZoom:18,attribution: osmAttrib});
-    map = new L.Map('map', {layers: [osm], center: new L.LatLng(19.04719036505186, 72.87094116210938), zoom: 11 });
+    map = new L.Map('map', {
+        layers: [osm],
+        center: new L.LatLng(19.04719036505186, 72.87094116210938),
+        zoom: 11,
+        editInOSMControl: true,        
+        editInOSMControlOptions: {
+            position: "topright",
+            iD: {
+                url: "myurl"
+            }
+        } 
+    });
     //console.log(map);
-    if (isMobile && $(window).width() < 700) { // dont show minimap on mobiles
+    if (!isMobile && $(window).width() > 700) { // dont show minimap on mobiles
         var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib});
         var miniMap = new L.Control.MiniMap(osm2).addTo(map);
     }
